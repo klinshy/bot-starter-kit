a4e2df43069bf92361d4754850fe2aba006f1d3f
klinshy <79143155+klinshy@users.noreply.github.com>
2024-10-23 16:07:19 +0000

    Deploying to gh-pages from master @ d8c2a9a89d5e5ae7b793ac4c767dfac5de61ea13 🚀

--- bot.js
+++ bot.js
@@ -1,11 +1,10 @@
-const l = {
-  run: async (i) => {
+const d = {
+  run: async (s) => {
     const r = {};
-    let s;
-    async function c(e, t) {
+    async function i(e) {
       try {
-        console.log(`Creating thread for bot: ${e}, user: ${t}`);
-        const o = await fetch(`https://ai.newit.works/api/v1/workspace/${e}/thread/new`, {
+        console.log(`Creating thread for bot: ${e}`);
+        const t = (await (await fetch(`https://ai.newit.works/api/v1/workspace/${e}/thread/new`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -14,19 +13,16 @@ const l = {
             Accept: "*/*"
           },
           body: JSON.stringify({ userId: 6 })
-        });
-        if (!o.ok)
-          throw new Error(`Failed to create thread: ${o.statusText}`);
-        const n = (await o.json()).thread.slug;
-        return r[t] = n, console.log(`Thread created with ID: ${n} for user ${t}`), n;
+        })).json()).thread.slug;
+        return console.log(t), t;
       } catch (o) {
         throw console.error("Failed to create thread:", o), o;
       }
     }
-    async function h(e, t, o) {
+    async function c(e, o, a) {
       try {
-        console.log(`Handling chat message for bot: ${e}, thread: ${t}, message: ${o}`), WA.chat.startTyping({ scope: "bubble" });
-        const a = await fetch(`https://ai.newit.works/api/v1/workspace/kos/thread/${t}/chat`, {
+        console.log(`Handling chat message for bot: ${e}, thread: ${o}, message: ${a}`), WA.chat.startTyping({ scope: "bubble" });
+        const t = await fetch(`https://ai.newit.works/api/v1/workspace/kos/thread/${o}/chat`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -39,32 +35,33 @@ const l = {
             mode: "chat",
             userId: 6
           })
-        }).then((d) => d.json()), n = a.textResponse;
+        }).then((h) => h.json()), n = t.textResponse;
         if (!n)
-          throw new Error("Custom AI returned no text response: " + JSON.stringify(a));
+          throw new Error("Custom AI returned no text response: " + JSON.stringify(t));
         console.log("Custom AI text response:", n), WA.chat.sendChatMessage(n, { scope: "bubble" }), WA.chat.stopTyping({ scope: "bubble" });
-      } catch (a) {
-        console.error("Failed to handle chat message:", a);
+      } catch (t) {
+        console.error("Failed to handle chat message:", t);
       }
     }
     WA.onInit().then(async () => {
-      console.log("COUCOU", i), s = await WA.player.name, WA.player.proximityMeeting.onParticipantJoin().subscribe(async (e) => {
-        console.log(`User ${e.name} with UUID ${e.uuid} joined the proximity meeting.`);
-        let t = r[e.uuid];
-        t ? console.log(`Found existing thread ${t} for user ${e.uuid}.`) : (console.log(`No existing thread for user ${e.uuid}, creating new thread.`), t = await c(s, e.uuid)), WA.chat.onChatMessage(
-          async (o, a) => {
-            if (!a.author) {
-              console.log("Received message with no author, ignoring.");
-              return;
-            }
-            console.log(`Received message from ${a.author.name}: ${o}`), await h(s, t, o);
-          },
-          { scope: "bubble" }
-        );
-      });
+      console.log("COUCOU", s);
+    }), WA.player.proximityMeeting.onParticipantJoin().subscribe(async (e) => {
+      console.log(`User ${e.name} with UUID ${e.uuid} joined the proximity meeting.`);
+      const o = await WA.player.name;
+      let a = r[e.uuid];
+      a ? console.log(`Found existing thread ${a} for user ${e.uuid}.`) : (console.log(`No existing thread for user ${e.uuid}, creating new thread.`), a = await i(o)), WA.chat.onChatMessage(
+        async (t, n) => {
+          if (!n.author) {
+            console.log("Received message with no author, ignoring.");
+            return;
+          }
+          console.log(`Received message from ${n.author.name}: ${t}`), await c(o, a, t);
+        },
+        { scope: "bubble" }
+      );
     });
   }
 };
 export {
-  l as default
+  d as default
 };
